81dd28972503af583873b9243884f22b50789be8
satishkumarsaride <satishkumarsaride192@gmail.com>
2021-06-21 00:12:30 +0530

    api url update

--- lib/apiCalls/apiUrl.dart
+++ lib/apiCalls/apiUrl.dart
@@ -6,11 +6,13 @@ class API {
   static var createOrder = '/api/order/Create-Ord/$uid'; //post with user id
   static var getOrders = '/api/order/user/$uid'; //get with user id
   static var particularOrder = '/api/order/orders/'; //get with order id
-  static var userRegister = '/api/user/newUser'; //post 
+  static var userRegister = '/api/user/newUser'; //post
   static var userDetails = '/api/user/users/$uid'; //get with user id
   static var editOrder = '/api/order/orders/'; //put with order id
   static var deleteOrder = '/api/order/orders/'; //delete with order id
   static var confirmOrder = '/api/order/orders/'; //put with order id
-  static var partnerDetails = '/api/partner/partners/';//get with partner id(pid) 
-  static var complaintOnPartner = '/api/partner/complaint';
+  static var partnerDetails ='/api/partner/partners/'; //get with partner id(pid)
+  static var complaintOnPartner = '/api/partner/complaint'; //post
+  static var report = '/api/partner/report'; //put
+  static var editPersonalInfo = '/api/user/users/$uid';//put
 }
