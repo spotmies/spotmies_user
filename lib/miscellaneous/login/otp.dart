a79df0cf1729793072a32d02474104742a1345a1
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-09-16 20:37:28 +0530

    otp bugs solved

--- lib/miscellaneous/login/otp.dart
+++ lib/miscellaneous/login/otp.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:developer';
+import 'dart:ui';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +9,7 @@ import 'package:pinput/pin_put/pin_put.dart';
 import 'package:provider/provider.dart';
 import 'package:spotmies/controllers/login_controller/login_controller.dart';
 import 'package:spotmies/providers/timer_provider.dart';
+import 'package:spotmies/utilities/snackbar.dart';
 import 'package:spotmies/views/home/navBar.dart';
 import 'package:spotmies/views/login/stepperPersonalInfo.dart';
 
@@ -20,6 +23,7 @@ class OTPScreen extends StatefulWidget {
 class _OTPScreenState extends State<OTPScreen> {
   // Timer _timer;
   // int _start = 60;
+  TimeProvider timerProvider;
   final GlobalKey<ScaffoldState> _scaffoldkey = GlobalKey<ScaffoldState>();
   String _verificationCode;
   final TextEditingController _pinPutController = TextEditingController();
@@ -33,14 +37,66 @@ class _OTPScreenState extends State<OTPScreen> {
       //const Color.fromRGBO(126, 203, 224, 1),
     ),
   );
-  @override
-  void initState() {
-    var timerProvider = Provider.of<TimeProvider>(context, listen: false);
+  startTimer() {
     Timer.periodic(Duration(seconds: 1), (timer) {
       timerProvider.updateTime();
+      if (timerProvider.countDown < 2) {
+        timer.cancel();
+        log("timer stopped");
+      }
     });
+  }
+
+  @override
+  void initState() {
+    timerProvider = Provider.of<TimeProvider>(context, listen: false);
+
     super.initState();
     _verifyPhone();
+  }
+
+  void resendOtp() {
+    timerProvider.resetTimer();
+    _verifyPhone();
+    snackbar(context, "Otp Resend successfully ");
+  }
+
+  loginUserWithOtp(otpValue) async {
+    log(otpValue.toString());
+    timerProvider.setLoader(true);
+    try {
+      await FirebaseAuth.instance
+          .signInWithCredential(PhoneAuthProvider.credential(
+              verificationId: _verificationCode, smsCode: otpValue))
+          .then((value) async {
+        if (value.user != null) {
+          // print("user already login");
+          bool resp = await checkUserRegistered(value.user.uid);
+          timerProvider.setLoader(false);
+          if (resp == false) {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => StepperPersonalInfo()),
+                (route) => false);
+          } else {
+            Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => GoogleNavBar()),
+                (route) => false);
+          }
+        } else {
+          timerProvider.setLoader(false);
+          snackbar(context, "Something went wrong");
+        }
+      });
+    } catch (e) {
+      FocusScope.of(context).unfocus();
+      log(e.toString());
+      timerProvider.setLoader(false);
+      snackbar(context, "Invalid OTP");
+      // _scaffoldkey.currentState
+      //     .showSnackBar(SnackBar(content: Text('invalid OTP')));
+    }
   }
 
   @override
@@ -54,127 +110,175 @@ class _OTPScreenState extends State<OTPScreen> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.blue[900],
       key: _scaffoldkey,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: _hight * 0.5,
+      body: Consumer<TimeProvider>(builder: (context, data, child) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Stack(children: [
+              AbsorbPointer(
+                absorbing: data.getLoader,
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: 50,
-                      child: Center(
-                          child: Icon(
-                        Icons.message,
-                        color: Colors.blue[900],
-                        size: 40,
-                      )),
+                    Container(
+                      height: _hight * 0.5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            radius: 50,
+                            child: Center(
+                                child: Icon(
+                              Icons.message,
+                              color: Colors.blue[900],
+                              size: 40,
+                            )),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Enter One Time Password You recieved to Verify',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            '+91 ${widget.phone}',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                        ],
+                      ),
                     ),
-                    SizedBox(
-                      height: 20,
+                    Container(
+                      height: _hight * 0.2,
+                      child: Padding(
+                        padding: const EdgeInsets.all(30.0),
+                        child: PinPut(
+                          fieldsCount: 6,
+                          textStyle: const TextStyle(
+                              fontSize: 25.0, color: Colors.blueGrey),
+                          eachFieldWidth: _width * 0.1,
+                          eachFieldHeight: _hight * 0.08,
+                          focusNode: _pinPutFocusNode,
+                          controller: _pinPutController,
+                          submittedFieldDecoration: pinPutDecoration,
+                          selectedFieldDecoration: pinPutDecoration,
+                          followingFieldDecoration: pinPutDecoration,
+                          pinAnimationType: PinAnimationType.fade,
+                          onChanged: (pin) {
+                            data.setOtp(pin.toString());
+                          },
+                          onSubmit: (pin) {
+                            loginUserWithOtp(pin);
+                          },
+                        ),
+                      ),
                     ),
-                    Text(
-                      'Enter One Time Password You recieved to Verify',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15,
-                          color: Colors.white),
+                    Container(
+                      width: 150,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor:
+                                MaterialStateProperty.all(Colors.white)),
+                        onPressed: () {
+                          loginUserWithOtp(data.getOtp);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Submit',
+                              style: TextStyle(
+                                  color: Colors.blue[900], fontSize: 18),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.check_circle,
+                              color: Colors.lightGreen,
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                    Text(
-                      '+91 ${widget.phone}',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20),
-                    ),
+                    data.countDown > 2
+                        ? Container(
+                            height: _hight * 0.3,
+                            child: CircularPercentIndicator(
+                                radius: 60,
+                                lineWidth: 5,
+                                animation: true,
+                                animationDuration: 99999,
+                                progressColor: Colors.white,
+                                percent: 1.0,
+                                backgroundColor: Colors.blue[900],
+                                center: Text(
+                                  '${data.countDown}',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20.0,
+                                      color: Colors.white),
+                                )),
+                          )
+                        : Container(
+                            padding: EdgeInsets.only(top: _hight * 0.1),
+                            width: 150,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all(Colors.white)),
+                              onPressed: resendOtp,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'Resend',
+                                    style: TextStyle(
+                                        color: Colors.blue[900], fontSize: 18),
+                                  ),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Icon(
+                                    Icons.change_circle,
+                                    color: Colors.lightGreen,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
                   ],
                 ),
               ),
-              Container(
-                height: _hight * 0.2,
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: PinPut(
-                    fieldsCount: 6,
-                    textStyle:
-                        const TextStyle(fontSize: 25.0, color: Colors.blueGrey),
-                    eachFieldWidth: _width * 0.1,
-                    eachFieldHeight: _hight * 0.08,
-                    focusNode: _pinPutFocusNode,
-                    controller: _pinPutController,
-                    submittedFieldDecoration: pinPutDecoration,
-                    selectedFieldDecoration: pinPutDecoration,
-                    followingFieldDecoration: pinPutDecoration,
-                    pinAnimationType: PinAnimationType.fade,
-                    onSubmit: (pin) async {
-                      try {
-                        await FirebaseAuth.instance
-                            .signInWithCredential(PhoneAuthProvider.credential(
-                                verificationId: _verificationCode,
-                                smsCode: pin))
-                            .then((value) async {
-                          if (value.user != null) {
-                            // print("user already login");
-                            bool resp =
-                                await checkUserRegistered(value.user.uid);
-                            if (resp == false) {
-                              Navigator.pushAndRemoveUntil(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          StepperPersonalInfo()),
-                                  (route) => false);
-                            } else {
-                              Navigator.pushAndRemoveUntil(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => GoogleNavBar()),
-                                  (route) => false);
-                            }
-                          }
-                        });
-                      } catch (e) {
-                        FocusScope.of(context).unfocus();
-                        // _scaffoldkey.currentState
-                        //     .showSnackBar(SnackBar(content: Text('invalid OTP')));
-                      }
-                    },
-                  ),
-                ),
+              Visibility(
+                visible: data.getLoader,
+                // visible: true,
+                child: Positioned.fill(
+                    child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
+                  child: Center(
+                      child: CircularProgressIndicator(
+                    strokeWidth: 6.0,
+                    backgroundColor: Colors.white,
+                  )),
+                )),
               ),
-            ],
-          ),
-          Container(
-            height: _hight * 0.3,
-            child: CircularPercentIndicator(
-                radius: 60,
-                lineWidth: 5,
-                animation: true,
-                animationDuration: 99999,
-                progressColor: Colors.white,
-                percent: 1.0,
-                backgroundColor: Colors.blue[900],
-                center: Consumer<TimeProvider>(builder: (context, data, child) {
-                  return Text(
-                    '${data.countDown}',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        color: Colors.white),
-                  );
-                })),
-          ),
-        ],
-      ),
+            ]),
+          ],
+        );
+      }),
     );
   }
 
   _verifyPhone() async {
+    startTimer();
     await FirebaseAuth.instance.verifyPhoneNumber(
         phoneNumber: '+91${widget.phone}',
         verificationCompleted: (PhoneAuthCredential credential) async {
@@ -195,7 +299,7 @@ class _OTPScreenState extends State<OTPScreen> {
           });
         },
         verificationFailed: (FirebaseAuthException e) {
-          print(e.message);
+          log(e.message.toString());
         },
         codeSent: (String verficationID, int resendToken) {
           setState(() {
