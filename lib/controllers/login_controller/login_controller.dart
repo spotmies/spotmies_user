2d8344cc6d6997e0dcb98ddc836b23027476b0e9
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-09-16 21:06:37 +0530

    otp timer bug solved

--- lib/controllers/login_controller/login_controller.dart
+++ lib/controllers/login_controller/login_controller.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mvc_pattern/mvc_pattern.dart';
+import 'package:provider/provider.dart';
 import 'package:spotmies/apiCalls/apiCalling.dart';
 import 'package:spotmies/apiCalls/apiUrl.dart';
 import 'package:spotmies/miscellaneous/login/otp.dart';
 import 'package:spotmies/models/loginModel.dart';
+import 'package:spotmies/providers/timer_provider.dart';
 
 class LoginPageController extends ControllerMVC {
+  TimeProvider timerProvider;
+
   var scaffoldkey = GlobalKey<ScaffoldState>();
   var formkey = GlobalKey<FormState>();
   var formkey1 = GlobalKey<FormState>();
@@ -17,9 +21,18 @@ class LoginPageController extends ControllerMVC {
   LoginPageController() {
     this.loginModel = LoginModel();
   }
+
+  @override
+  void initState() {
+    timerProvider = Provider.of<TimeProvider>(context, listen: false);
+
+    super.initState();
+  }
+
   dataToOTP() {
     if (formkey.currentState.validate()) {
       formkey.currentState.save();
+      timerProvider.resetTimer();
       Navigator.of(context).push(
           MaterialPageRoute(builder: (context) => OTPScreen(loginnum.text)));
     }
