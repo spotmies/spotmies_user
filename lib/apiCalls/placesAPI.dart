a85f4931989295ef90bd9db47beeb85adefbab42
modernsilpi <modernsilpi@gmail.com>
2021-10-21 10:20:08 +0530

    locations search  modified

--- lib/apiCalls/placesAPI.dart
+++ lib/apiCalls/placesAPI.dart
@@ -30,4 +30,35 @@ class PlacesApi {
       throw Exception();
     }
   }
+
+  static Future<List> getAllLocations() async {
+    final url =
+        Uri.parse('https://spotmiesserver.herokuapp.com/api/geocode/all');
+    final response = await http.get(url);
+    // List data  = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      final List geoLocations = json.decode(response.body);
+      log('API Hit');
+      log(geoLocations.toString());
+
+      return geoLocations;
+    } else {
+      throw Exception();
+    }
+  }
+}
+
+getArea(String searchAreaName, List geoLocationsList) {
+  print(searchAreaName);
+  return geoLocationsList.where((geo) {
+    final subLocality = geo['subLocality'].toLowerCase();
+    final locality = geo['addressLine'].toLowerCase();
+    final coord = geo['coordinates'].toString();
+    final searchLower = searchAreaName.toLowerCase();
+
+    return subLocality.contains(searchLower) ||
+        locality.contains(searchLower) ||
+        coord.contains(searchLower);
+  }).toList();
 }
