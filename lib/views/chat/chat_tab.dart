745a9990216c5b62025bf29c5b5fcf1622f5c151
modernsilpi <modernsilpi@gmail.com>
2021-10-28 18:32:59 +0530

    chat tab slide work with lastmodified

--- lib/views/chat/chat_tab.dart
+++ lib/views/chat/chat_tab.dart
@@ -1,6 +1,11 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:spotmies/providers/chat_provider.dart';
+import 'package:spotmies/providers/responses_provider.dart';
 import 'package:spotmies/utilities/appConfig.dart';
 import 'package:spotmies/views/chat/chatapp/chat_list.dart';
 import 'package:spotmies/views/chat/response.dart';
@@ -11,7 +16,10 @@ class Chat extends StatefulWidget {
 }
 
 class _ChatState extends State<Chat> {
+  ResponsesProvider responseProvider;
+  ChatProvider chatProvider;
   var name = 'Response';
+  int initialTabIndex = 0;
   var list = [
     Center(
       child: Responsee(),
@@ -20,9 +28,40 @@ class _ChatState extends State<Chat> {
       child: ChatList(),
     ),
   ];
+  void initState() {
+    chatProvider = Provider.of<ChatProvider>(context, listen: false);
+    responseProvider = Provider.of<ResponsesProvider>(context, listen: false);
+    setTabIndex();
+    super.initState();
+  }
+
+  void setTabIndex() {
+    if (responseProvider.responsesList.length < 1) {
+      setState(() {
+        initialTabIndex = 1;
+      });
+    } else if (chatProvider.chatList.length < 1) {
+      setState(() {
+        initialTabIndex = 0;
+      });
+    } else {
+      if (responseProvider.responsesList[0]['join'] >
+          chatProvider.chatList[0]['lastModified']) {
+        setState(() {
+          initialTabIndex = 0;
+        });
+      } else {
+        setState(() {
+          initialTabIndex = 1;
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+        initialIndex: initialTabIndex,
         length: 2,
         child: Scaffold(
           appBar: AppBar(
